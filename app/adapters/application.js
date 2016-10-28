8873015d7e6c9a1760e917839c387e077365903d
TheNeilist <hireahoosier@yahoo.com>
2016-10-28 16:08:58 -0400

    try the api public domain

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
 //    host: 'http://localhost:8090',
-    host: 'http://127.0.0.1:8090',
+    host: 'http://ec2-52-43-138-208.us-west-2.compute.amazonaws.com:8090',
     headers: {
      'Accept': 'application/json'
     }
