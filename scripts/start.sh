e188565d189f4dbbadc9a7fbbd36c3fe2ab65faa
TheNeilist <hireahoosier@yahoo.com>
2016-10-28 11:43:44 -0400

    fix start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 BUILD_ID=pleaseDontKillMeUI
 cd /var/lib/jenkins/workspace/everwas-ui;
-nohup nohup > log.txt 2> errors.txt < /dev/null &
+nohup nohup ember s > log.txt 2> errors.txt < /dev/null &
 PID_EVERWAS_UI=$!
 echo $PID_EVERWAS_UI > /tmp/everwas-ui-pid
 echo "started everwas-ui PID $PID_EVERWAS_UI"
