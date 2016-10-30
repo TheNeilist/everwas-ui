dad7b4de486eb97b56c765c1244f3108afd94a23
TheNeilist <hireahoosier@yahoo.com>
2016-10-29 21:45:31 -0400

    render time periods on time tree

--- app/routes/timetree.js
+++ app/routes/timetree.js
@@ -1,19 +1,83 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   model() {
+
+      return this.get('store').findAll('timeline').then(function(timelines) {
+
+        var jsonTimelineArray = timelines.map(function(record) {
+          return record.toJSON();
+        });
+
+        var timetree = [];
+        for(var i = 0; i < jsonTimelineArray.length; i++) {
+            var jsonTimeline = jsonTimelineArray[i];
+            var timePeriods = jsonTimeline.timePeriods;
+            var sections = [];
+            var startTimes = [];
+            var endTimes = [];
+            if (timePeriods.length > 0) {
+              for(var s = 0; s < timePeriods.length; s++) {
+                var timePeriod = timePeriods[s];
+                var startTime = convertStringToDate(timePeriod.periodStart);
+                var endTime = convertStringToDate(timePeriod.periodEnd)
+                sections.push({
+                  start: startTime.getTime(),
+                  end: endTime.getTime()
+                });
+                startTimes.push(startTime);
+                endTimes.push(endTime);
+              }
+              var minTime = minDate(startTimes);
+              var maxTime = maxDate(endTimes);
+
+              timetree.push({
+                label: jsonTimeline.category.name,
+                start: minTime.getTime(),
+                end: maxTime.getTime(),
+                sections: sections
+              });
+            }
+        }
+        return timetree;
+      });
+
+//    return mockTimeline();
+
+  }
+
+});
+
+function convertStringToDate(strDate) {
+ return new Date(strDate);
+}
+
+function minDate(dates) {
+  return new Date(Math.min.apply(null,dates));
+}
+
+function maxDate(dates) {
+  return new Date(Math.max.apply(null,dates));
+}
+
+function mockTimeline() {
 
       var timetree1 = {
         label: 'One',
         start: 0,
-        end: 100
+        end: 200,
+        sections: [
+          {start: 0, end: 50},
+          {start: 100, end: 150}
+        ]
       };
 
       var timetree2 = {
-              label: 'Two',
-              start: 50,
-              end: 150
-            };
+        label: 'Two',
+        start: 50,
+        end: 150
+      };
 
       var timetree3 = {
         label: 'Two',
@@ -28,6 +92,4 @@ export default Ember.Route.extend({
       };
 
       return [timetree1, timetree2, timetree3, timetree4];
-
-  }
-});
+}
