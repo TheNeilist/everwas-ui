008a9714f17b14abbf2185377bfd895ec3e52d84
TheNeilist <hireahoosier@yahoo.com>
2016-12-19 16:50:28 -0500

    login phase 1

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,14 +1,35 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Controller.extend({
 
   actions: {
-    login: function() {
-      var self = this;
-      //TODO: authenticate against api
 
-      self.transitionToRoute('timeline');
+    login: function() {
+
+      var self = this;
+
+      var authCreds = JSON.stringify({
+          username: this.username,
+          password: this.password
+      });
+
+      Ember.$.ajax({
+          url: config.APP.API_HOST + '/user',
+          dataType   : 'json',
+          contentType: 'application/json; charset=UTF-8',
+          data: authCreds,
+          type: 'POST',
+          contentType: 'application/json'
+       }).then(function(responseObject/*, statusText, jqXHR*/) {
+          //TODO: do something with the user so it is visible in templates, other controllers, etc
+          self .transitionToRoute('timeline');
+       }, function(jqXHR/*, textStatus, errorThrown*/) {
+          //TODO: implement sexy alert
+          alert('Authentication failed.')
+       });
     }
+
   }
 
 });
