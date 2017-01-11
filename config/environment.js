f74ceeb826f76859b6e002b330db467902d5b25f
TheNeilist <hireahoosier@yahoo.com>
2017-01-11 16:52:01 -0500

    update api host url

--- config/environment.js
+++ config/environment.js
@@ -26,7 +26,7 @@ module.exports = function(environment) {
      ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
      ENV.APP.LOG_VIEW_LOOKUPS = true;
 
-     ENV.APP.API_HOST = 'http://localhost:8090';
+     ENV.APP.API_HOST = 'http://127.0.0.1:8090';
   }
 
   if (environment === 'test') {
@@ -39,11 +39,11 @@ module.exports = function(environment) {
 
     ENV.APP.rootElement = '#ember-testing';
 
-    ENV.APP.API_HOST = 'http://www.theneilist.com:8090';
+    ENV.APP.API_HOST = 'http://127.0.0.1:8090';
   }
 
   if (environment === 'production') {
-    ENV.APP.API_HOST = 'http://www.theneilist.com:8090';
+    ENV.APP.API_HOST = 'http://127.0.0.1:8090';
   }
 
   return ENV;
